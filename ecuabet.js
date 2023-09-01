99549df979bc76cf9a8bb1cf57d4cbf4982b430a
DTI <dti@alacoohperu.pe>
2023-09-01 11:51:46 -0500

    first commit

---
+++ ecuabet.js
@@ -0,0 +1,38 @@
+const URI = "https://apiecuabet.alacoohecuador.com/api/ecuabet";
+var content = document.getElementById('contenido');
+var content2 = document.getElementById('contenido2');
+var content3 = document.getElementById('contenido3');
+const URL = window.location.href;
+
+async function init() {
+  await getUser();
+  setTimeout(() => {
+    content.style.display = "none";
+    content3.style.display = "block";
+    content2.style.display = "none";
+  }, 7500);
+}
+
+init();
+async function getUser(){
+
+  const resp = await axios.get(URI);
+  const xmlData = resp.data.data;
+
+  console.log(xmlData)
+
+  if (xmlData) {
+    // America
+    const dataItem1  = xmlData.competidores[0].Name;
+    const dataItem2  = xmlData.competidores[1].Name;
+    const dataItem3  = xmlData.cuotas[0].Price.toFixed(2);
+    const dataItem4  = xmlData.cuotas[1].Price.toFixed(2);
+    const dataItem5  = xmlData.cuotas[2].Price.toFixed(2);
+    
+    document.getElementById('title1').innerHTML = dataItem1;
+    document.getElementById('title2').innerHTML = dataItem2;
+    document.getElementById('cuota1').innerHTML = dataItem3;
+    document.getElementById('cuota2').innerHTML = dataItem4;
+    document.getElementById('cuota3').innerHTML = dataItem5;
+  }
+}
