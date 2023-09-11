863bd4c02e1770957225cc317b503afb8c60a628
DTI <dti@alacoohperu.pe>
2023-09-11 15:52:12 -0500

    cambio

--- ecuabet.js
+++ ecuabet.js
@@ -1,55 +1,37 @@
-const URI = "https://apibeeyond.alacoohperu.pe/api/ecuabet";
+const URI = "https://apiecuabet.alacoohecuador.com/api/ecuabet";
 var content = document.getElementById('contenido');
 var content2 = document.getElementById('contenido2');
 var content3 = document.getElementById('contenido3');
-var reproductor = document.getElementById("reproductor");
-
 const URL = window.location.href;
 
 async function init() {
   await getUser();
-  // Play function to start the content exhibition
-  function play() {
-    console.log('Playing content');
-    // Get video element and play it if exists
-    var video = document.getElementsByTagName('video')[0];
-    if (video) {
-      video.play();
-    }
-  }
-
-  // This function is called when the content is exhibited on a BroadSign player
-  function BroadSignPlay() {
-    play();
-  }
-
-  // If the content is loaded on a BroadSign player, but is inside an iframe, we need make a redirect to the parent window
-  if (typeof BroadSignObject != 'undefined') {
-    // If this content is loaded inside an iframe
-    if (window.self !== window.top) {
-      // Put this content in the top window
-      window.top.location.href = window.self.location.href;
-    }
-  }
-
-  // If this page is loaded Broadsign Player, play the content instantly
-  if (typeof BroadSignObject === 'undefined') {
-    play();
-  }
+  setTimeout(() => {
+    content2.style.display = "block";
+  }, 7500);
 }
 
 init();
-
-async function getUser() {
+async function getUser(){
 
   const resp = await axios.get(URI);
-  const xmlData = resp.data.entry;
+  const xmlData = resp.data.data;
 
   console.log(xmlData)
 
-  reproductor.innerHTML = `<video muted="" preload="auto" ><source src="${xmlData}" type="video/mp4"></video>`;  // if (xmlData) {
-  //   // America
-  //   const dataItem1  = xmlData.competidores[0].Name;
-  //   document.getElementById('cuota3').innerHTML = dataItem5;
-  // }
+  if (xmlData) {
+    // America
+    const dataItem1  = xmlData.competidores[0].Name;
+    const dataItem2  = xmlData.competidores[1].Name;
+    const dataItem3  = xmlData.cuotas[0].Price.toFixed(2);
+    const dataItem4  = xmlData.cuotas[1].Price.toFixed(2);
+    const dataItem5  = xmlData.cuotas[2].Price.toFixed(2);
+    
+    document.getElementById('title1').innerHTML = "";
+    document.getElementById('title2').innerHTML = "";
+    document.getElementById('title3').innerHTML = "";
+    document.getElementById('cuota1').innerHTML = dataItem3;
+    document.getElementById('cuota2').innerHTML = dataItem4;
+    document.getElementById('cuota3').innerHTML = dataItem5;
+  }
 }
