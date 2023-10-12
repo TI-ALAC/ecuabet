b0570a2e21e4eb250899c78d95ba1532f9e41b48
DTI <dti@alacoohperu.pe>
2023-10-12 11:29:24 -0500

    cambio img

--- ecuabet.js
+++ ecuabet.js
@@ -1,4 +1,4 @@
-const URI = "https://apiecuabet.alacoohecuador.com/api/ecuabet";
+const URI = "http://localhost:4002/api/ecuabet";
 var content = document.getElementById('contenido');
 var content2 = document.getElementById('contenido2');
 var content3 = document.getElementById('contenido3');
@@ -17,15 +17,15 @@ async function getUser(){
   const resp = await axios.get(URI);
   const xmlData = resp.data.data;
 
-  console.log(xmlData)
+  console.log(xmlData.cuotas[0])
 
   if (xmlData) {
     // America
     const dataItem1  = xmlData.competidores[0].Name;
     const dataItem2  = xmlData.competidores[1].Name;
-    const dataItem3  = xmlData.cuotas[0].Price.toFixed(2);
-    const dataItem4  = xmlData.cuotas[1].Price.toFixed(2);
-    const dataItem5  = xmlData.cuotas[2].Price.toFixed(2);
+    const dataItem3  = xmlData.cuotas[0].price.toFixed(2);
+    const dataItem4  = xmlData.cuotas[1].price.toFixed(2);
+    const dataItem5  = xmlData.cuotas[2].price.toFixed(2);
     
     document.getElementById('title1').innerHTML = "";
     document.getElementById('title2').innerHTML = "";
